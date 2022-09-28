c719d068f5c55d43819ff5bd7c9aff5997795697
LorenzoBaridotti <lorenzo.baridotti@hotmail.com>
2022-09-27 23:28:08 -0300

    Exercícios a027

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const frutas = ["laranja", "limão", "uva"];
+
+for (let i = 0; i < frutas.length; i++) {
+  const addFrutas = document.getElementById(`fruta${i + 1}`);
+  addFrutas.innerHTML += frutas[i];
+}
+
+const valorFruta = document.getElementById("fruta");
+
+const imprimirFruta = () => {
+  console.log(valorFruta.value);
+};
+
+const addFruta = () => {
+  fruta4.innerHTML = valorFruta.value;
+  valorFruta.value = "";
+};
